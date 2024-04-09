deb86f6857f3e1a70a585bbf60a0b6b2c3765845
Victor Contreras <contreras3991@gmail.com>
2024-04-09 18:30:32 -0300

    adicionado mais uma rota

--- main.py
+++ main.py
@@ -33,3 +33,10 @@ def ola_mundo(): # Response
 @app.get("/produtos")
 def listar_produtos():
     return produtos
+
+@app.get("/produtos/{id}")
+def buscar_produto(id: int):
+    for produto in produtos:
+        if produto['id'] == id:
+            return produto
+    return {"Status": 404, "Mensagem": "Produto nãoe encontrado"}
